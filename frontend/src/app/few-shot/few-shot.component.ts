98b1a71bf6f782a7932e8594e1d29d2ee2d3e600
Ramesh SathiyaNarayanan <rameshsathiyanarayanan@gmail.com>
2026-03-02 00:22:45 -0600

    Few shot tested and verified

--- frontend/src/app/few-shot/few-shot.component.ts
+++ frontend/src/app/few-shot/few-shot.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ApiserviceService } from '../services/apiservice.service';
 
 @Component({
   selector: 'app-few-shot',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './few-shot.component.html',
   styleUrl: './few-shot.component.css'
 })
