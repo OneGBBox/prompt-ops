9acbc15d95f4b7c269a813e5422cde647fb3735b
Ramesh SathiyaNarayanan <rameshsathiyanarayanan@gmail.com>
2026-03-02 00:23:20 -0600

    Json schema working as expected

--- frontend/src/app/json-schema/json-schema.component.ts
+++ frontend/src/app/json-schema/json-schema.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ApiserviceService } from '../services/apiservice.service';
 
 @Component({
   selector: 'app-json-schema',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './json-schema.component.html',
   styleUrl: './json-schema.component.css'
 })
